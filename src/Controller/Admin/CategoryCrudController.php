7eabc65548f7d619aa0a80efb646dfc221d5a41b
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-17 17:41:08 -0400

    Set up view of entity mannager

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CategoryCrudController extends AbstractCrudController
@@ -12,14 +15,18 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setSearchFields(['name'])
+            ->setDefaultSort(['id' => 'DESC']);
+    }
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('name', 'Name'),
+            TextField::new('slug', 'Slug'),
         ];
     }
-    */
 }
