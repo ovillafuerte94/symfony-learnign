58cfc07d30867da230f2141aba0f70aca691a5d9
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-17 17:13:46 -0400

    Add options to entities in side menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Controller\Admin\PostCrudController;
+use App\Entity\Category;
+use App\Entity\Comment;
+use App\Entity\Post;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -28,7 +31,12 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+
+        yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Posts', 'fas fa-list', Post::class);
+        yield MenuItem::linkToCrud('Comments', 'fas fa-list', Comment::class);
+
+        // yield MenuItem::linkToRoute('Web site', 'fa fa-home', 'app_home');
     }
 }
