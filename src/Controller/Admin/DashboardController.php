f811a49d145cd670373261ec2e56a852c80e7cb6
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-17 17:13:07 -0400

    Set title of dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -23,7 +23,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Symfony Learning');
+            ->setTitle('Symfony');
     }
 
     public function configureMenuItems(): iterable
