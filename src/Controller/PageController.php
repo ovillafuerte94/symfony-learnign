cb9551962fb9907829dedef8dfcbc2924f58d022
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-18 01:25:54 -0400

    Add comment form

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,6 +22,11 @@ class PageController extends AbstractController
     #[Route('/blog/{slug}', name: 'app_post')]
     public function post(Post $post): Response
     {
-        return $this->render('page/post.html.twig', ['post' => $post]);
+        $form = $this->createForm(CommentType::class);
+        
+        return $this->render('page/post.html.twig', [
+            'post' => $post,
+            'form' => $form->createView()
+        ]);
     }
 }
